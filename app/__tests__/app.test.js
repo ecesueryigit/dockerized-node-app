e252d88f056fe276f68a4c3feb4ef082a71d06c6
ecesueryigit <ecesueryigit@gmail.com>
2026-04-18 00:46:02 +0300

    __tests__ moved

---
+++ app/__tests__/app.test.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const app = require("../app/index");
+
+describe("API Tests", () => {
+  it("GET / should return 200", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("GET /health should return ok", async () => {
+    const res = await request(app).get("/health");
+    expect(res.body.status).toBe("ok");
+  });
+
+  it("GET /todos should return array", async () => {
+    const res = await request(app).get("/todos");
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
+  it("POST /todos should create a new todo", async () => {
+    const res = await request(app)
+      .post("/todos")
+      .send({ title: "learn testing" });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("id");
+    expect(res.body.title).toBe("learn testing");
+    expect(res.body.completed).toBe(false);
+
+    console.log(res.statusCode);
+    console.log(res.body);
+
+  });
+
+  it("POST /todos should return 400 if title is missing", async () => {
+  const res = await request(app).post("/todos").send({});
+
+  expect(res.statusCode).toBe(400);
+  expect(res.body.error).toBe("title is required");
+  });
+});
