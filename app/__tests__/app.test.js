9aad5f96cf72d487a35b5742dfb0d4171659e704
ecesueryigit <ecesueryigit@gmail.com>
2026-04-18 00:53:01 +0300

    change index.js

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app/index");
+const app = require("../index");
 
 describe("API Tests", () => {
   it("GET / should return 200", async () => {
