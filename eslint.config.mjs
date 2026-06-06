38ecb6b0c6df9a7004676db968dc75588ac1ae34
ecesueryigit <ecesueryigit@gmail.com>
2026-06-06 16:52:02 +0300

    update lint file

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
