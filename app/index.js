27c88eda10501fbe5980de2ce5a4bbc1828d0c4f
ecesueryigit <ecesueryigit@gmail.com>
2026-06-23 23:18:16 +0300

    connected mongodb to todos app

--- app/index.js
+++ app/index.js
@@ -4,9 +4,9 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Mongo error:", err));
 
+const Todo = require("./models/Todo");
+
 const express = require("express");
-const fs = require("fs");
-const path =require("path");
 
 const app = express();
 app.use(express.json());
@@ -15,21 +15,6 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.NODE_ENV || "development";
 
-const dataDir = path.join(__dirname, "data");
-
-
-if(!fs.existsSync(dataDir)) {
-  fs.mkdirSync(dataDir, { recursive: true, mode: 0o777  });
-}
-
-const DATA_FILE = path.join(dataDir, "todos.json");
-
-let todos = [];
-
-if (fs.existsSync(DATA_FILE)) {
-	const data = fs.readFileSync(DATA_FILE);
-	todos = JSON.parse(data);
-}
 
 app.get("/", (req, res) => {
   res.send(`Hello DevOps from ${ENV} environment`);
@@ -40,25 +25,25 @@ app.get("/health", (req, res) => {
 });
 
 
-app.get("/todos" , (req, res) => {
+app.get("/todos" , async (req, res) => {
+  const todos= await Todo.find();
 	res.json(todos);
 });
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
 
   if (!req.body.title) {
     return res.status(400).json({ error: "title is required" });
   }
 
   try{
-    const newTodo = {
-      id: todos.length + 1,
+    const newTodo =  await Todo.create({
+     
       title: req.body.title,
       completed: false
-    };
+    });
 
-    todos.push(newTodo);
-    fs.writeFileSync(DATA_FILE, JSON.stringify(todos));
+    
 
     res.status(201).json(newTodo);
   } catch (err) {
@@ -66,6 +51,11 @@ app.post("/todos", (req, res) => {
     res.status(500).json({ error: err.message });
     }
 
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "deleted" });
 });
 
 module.exports = app;
