4ee8a1e1a4f3644105acb6ade5668d9c2383384b
ecesueryigit <ecesueryigit@gmail.com>
2026-07-14 20:56:20 +0300

    removed unnecessary env variable

--- app/index.js
+++ app/index.js
@@ -16,8 +16,6 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-
-const PORT = process.env.PORT || 3000;
 const ENV = process.env.NODE_ENV || "development";
 
 
