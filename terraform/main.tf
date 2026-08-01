ef6883e6bdfc8d6bb689498fa507603660fc9be3
ecesueryigit <ecesueryigit@gmail.com>
2026-08-01 22:31:22 +0300

    Add Terraform AWS security group resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,22 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_security_group" "terraform_test_sg" {
+  name        = "terraform-test-sg"
+  description = "Created by Terraform learning"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
