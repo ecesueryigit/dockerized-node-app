7f4bd1db3d33b64f87d8e710fb861aadea6224bb
ecesueryigit <ecesueryigit@gmail.com>
2026-08-11 00:45:08 +0300

    added security group rules

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,3 +1,33 @@
 resource "aws_security_group" "existing_sg" {
-    
+  name        = "dockerized-node-app-sg"
+  description = "Security Group for dockerized-node-app application EC2"
+  vpc_id      = data.aws_subnet.existing.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["176.233.30.102/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
